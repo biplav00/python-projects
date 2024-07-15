fa2602715098c8ad2ca88708d9776eecbba90c11
biplav00 <biplav00@gmail.com>
2024-07-15 10:06:28 +0545

    Wtf did I do???

--- calculator/main.py
+++ calculator/main.py
@@ -1,15 +1,10 @@
-import tkinter as tk
-from PIL import Image, ImageTk
 
+import customtkinter as ctk
+from pytube import YouTube
 
-root = tk.Tk()
-root.title("Weather App")
-root.geometry("900x600")
-img = Image.open('bg.png')
-img = img.resize((900, 600), Image.ANTIALIAS)
-img_photo = ImageTk.PhotoImage(img)
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
 
-bg_lbl = tk.Label(root, image_photo)
-bg_lbl.place(x=0, y=0, width=900, height=500)
-
-root.mainloop()
+app = ctk.CTk()
+app.geometry("500x500")
+app.mainloop()
